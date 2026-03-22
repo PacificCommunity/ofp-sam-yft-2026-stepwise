6468af9a15bf151b0f2a4f31872c6b538a75854e
Arni Magnusson <thisisarni@gmail.com>
2026-03-23 08:00:16 +1100

    Add 02b_Increase_Specs and 02c_2278e

--- R/01_run_models.R
+++ R/01_run_models.R
@@ -11,3 +11,5 @@ full_submit("01_Diag2023/01a_Online_Repo", "2.2.2.0_d")
 
 # 02_NewExe
 full_submit("02_NewExe/02a_New_Exe", "2.2.7.8_d")
+full_submit("02_NewExe/02b_Increase_Specs", "2.2.7.8_d")
+full_submit("02_NewExe/02c_2278e", "2.2.7.8_e")
