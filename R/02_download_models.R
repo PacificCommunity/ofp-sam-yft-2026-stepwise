ad36693627af33da5b2a9a8781facf6d12cbc1bf
Arni Magnusson <thisisarni@gmail.com>
2026-03-23 14:54:28 +1100

    Download from Condor to laptop

--- R/02_download_models.R
+++ R/02_download_models.R
@@ -8,6 +8,9 @@ session <- ssh_connect("NOUOFPSUBMIT")
 (jobs <- condor_dir(sort="dir"))
 models <- jobs$dir[jobs$status=="finished"]
 
+################################################################################
+# Copy from Condor to Penguin
+
 # Prepare SCP commands
 from <- file.path("condor", models)
 to <- "penguin:yft/2026/model_runs/stepwise"
@@ -19,3 +22,13 @@ for(i in seq_along(from))
   message("Copying '", models[i], "'")
   try(ssh_exec_stdout(cmd[i]))
 }
+
+################################################################################
+# Download from Condor to laptop
+
+# Destination folders, will be created by full_download() below
+folders <- file.path("~/x/yft/2026/model_runs/stepwise", models)
+
+# Download results
+for(i in seq_along(folders))
+  try(full_download(folders[i]))
